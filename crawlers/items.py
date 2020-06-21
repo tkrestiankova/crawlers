c8d652a291e51389e83011de6e7cac67678a7c85
Tamara Krestiankova <tamara.krestiankova@kiwi.com>
2020-06-21 18:31:12 +0200

    Update if profile exists

--- crawlers/items.py
+++ crawlers/items.py
@@ -16,16 +16,32 @@ class Profile(scrapy.Item):
     phone = scrapy.Field()
     website = scrapy.Field()
 
+    def _exists(self) -> bool:
+        """
+            Determine if profile already exists
+        """
+        existing_profiles = queries.profiles_get()
+        if any([ep["name"] == self["name"] for ep in existing_profiles]):
+            return True
+        return False
+
     def save(self) -> None:
         """
             Save Yelp profile
         """
         if settings.is_prod_env():
-            queries.profiles_add(profile={
-                "name": self["name"],
-                "phone": self["phone"],
-                "website": self["website"]
-            })
+            if not self._exists():
+                queries.profiles_add(profile={
+                    "name": self["name"],
+                    "phone": self["phone"],
+                    "website": self["website"]
+                })
+            else:
+                queries.profiles_update(profile={
+                    "name": self["name"],
+                    "phone": self["phone"],
+                    "website": self["website"]
+                })
         else:
             ts = repr(time.time())[:10]
             with open(f"crawlers/results/{ts}.json", "w+") as new_file:
